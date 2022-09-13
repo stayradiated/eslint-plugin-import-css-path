455c81167819a5fd6e81b48b00b3ef62f0f352ae
Aaron Carlino <unclecheese@leftandmain.com>
2022-09-13 17:27:09 +1200

    Add new config option for path alias

--- src/exists.ts
+++ src/exists.ts
@@ -1,23 +1,69 @@
-import path from 'path'
+import path from "path"
 
 type Node = {
   arguments: readonly {
-    value: string,
-  }[],
+    value: string
+  }[]
   callee: {
-    name: string,
-  },
+    name: string
+  }
   source: {
-    value: string,
-  },
+    value: string
+  }
 }
 
 type Context = {
-  getFilename: () => string,
-  report: (node: Node, message: string, options: {}) => void,
+  getFilename: () => string
+  report: (node: Node, message: string, options: {}) => void
+  options: [Settings]
 }
 
-const getCurrentDirectory = (context: Context) => {
+type Settings = {
+  resolve?: {
+    [key: string]: string
+  }
+}
+
+const defaultSettings = {}
+
+const getSettings = (context: Context): Context["options"][0] => {
+  if (context && context.options && typeof context.options[0] === "object") {
+    return { ...defaultSettings, ...context.options[0] }
+  }
+
+  return defaultSettings
+}
+
+const applyResolve = (
+  resolve: NonNullable<Settings["resolve"]>,
+  context: Context,
+  absPath: string
+) => {
+  if (!absPath.includes("~")) {
+    return absPath
+  }
+  const entries = Object.entries(resolve)
+  for (const mapping of entries) {
+    const [testRx, alias] = mapping
+    const rx = new RegExp(testRx)
+    if (rx.test(absPath)) {
+      const replaced = absPath.replace(rx, alias)
+      const resolved = path.relative(
+        path.dirname(context.getFilename()),
+        replaced
+      )
+      // path.relative doesn't add the ./ prefix for peers or descendants
+      if (!resolved.startsWith(".")) {
+        return `./${resolved}`
+      }
+      return resolved
+    }
+  }
+
+  return absPath
+}
+
+const getCurrentDirectory = (context: Context, node: Node) => {
   let filename = context.getFilename()
   if (!path.isAbsolute(filename)) {
     filename = path.join(process.cwd(), filename)
@@ -27,30 +73,34 @@ const getCurrentDirectory = (context: Context) => {
 }
 
 const testRequirePath = (fileName: string, node: Node, context: Context) => {
-  if (fileName.endsWith('.css') === false) {
+  if (fileName.endsWith(".css") === false) {
     return
   }
-
-  const currentDir = getCurrentDirectory(context)
+  let resolvedFile = fileName
+  const currentDir = getCurrentDirectory(context, node)
+  const settings = getSettings(context)
+  if (settings.resolve) {
+    resolvedFile = applyResolve(settings.resolve, context, resolvedFile)
+  }
 
   try {
-    require.resolve(fileName, { paths: [currentDir] })
+    require.resolve(resolvedFile, { paths: [currentDir] })
   } catch (e) {
-    context.report(node, `Cannot find module: ${fileName}`, {})
+    context.report(node, `Cannot find module: ${resolvedFile}`, {})
   }
 }
 
 const exists = (context: Context) => {
   return {
-    ImportDeclaration (node: Node) {
+    ImportDeclaration(node: Node) {
       testRequirePath(node.source.value, node, context)
     },
 
-    CallExpression (node: Node) {
+    CallExpression(node: Node) {
       if (
-        node.callee.name !== 'require' ||
+        node.callee.name !== "require" ||
         !node.arguments.length ||
-        typeof node.arguments[0].value !== 'string' ||
+        typeof node.arguments[0].value !== "string" ||
         !node.arguments[0].value
       ) {
         return
